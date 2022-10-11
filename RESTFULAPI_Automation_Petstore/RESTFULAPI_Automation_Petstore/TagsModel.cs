e240d42ce49cd698c835a0f7409e464c549181de
Katrina Almaria <Katrina.Almaria@ey.com>
2022-10-12 01:29:08 +0800

    Restful API Test for Pet Store

---
+++ RESTFULAPI_Automation_Petstore/RESTFULAPI_Automation_Petstore/TagsModel.cs
@@ -0,0 +1,16 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RESTFULAPI_Automation_Petstore
+{
+    public class TagsModel
+    {
+        [JsonProperty("id")]
+        public long Id { get; set; }
+
+        [JsonProperty("name")]
+        public string Name { get; set; }
+    }
+}
